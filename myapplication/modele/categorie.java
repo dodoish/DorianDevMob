056350418ba5b074beabaeaef9974942c20b4b3f
dodoish <50076582+dodoish@users.noreply.github.com>
2020-11-29 14:06:13 +0100

    Update categorie.java

--- myapplication/modele/categorie.java
+++ myapplication/modele/categorie.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.modele;
 
-public class categorie {
+import java.io.Serializable;
+
+public class categorie implements Serializable {
     private int id;
     private String titre;
     private String Visuel;
